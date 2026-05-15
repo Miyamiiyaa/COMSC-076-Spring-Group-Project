ad32544ba21f84f383bf55d39b659125f780a5eb
evanhaa <evantrha@gmail.com>
2026-05-14 23:46:00 -0700

    formatting fix

--- src/library/Library.java
+++ src/library/Library.java
@@ -13,183 +13,178 @@ import java.io.PrintWriter;
  */
 public class Library {
 
-    // Using HashMap as the data structure for storing books in the library
-    HashMap<String, Book> books = new HashMap<>(); // key is ISBN of the book
+	// Using HashMap as the data structure for storing books in the library
+	HashMap<String, Book> books = new HashMap<>(); // key is ISBN of the book
 
-    /**
-     * @return the number of books (not number of copies) in the library.
-     */
-    public int getNumberOfBooks() {
-        return books.size(); // returns the number of books, excluding multiple
-                             // copies of each book.
-    }
+	/**
+	 * @return the number of books (not number of copies) in the library.
+	 */
+	public int getNumberOfBooks() {
+		return books.size(); // returns the number of books, excluding multiple
+								// copies of each book.
+	}
 
-    /**
-     * Adds a book to the library. If the library already has this book then it
-     * adds the number of copies the library has.
-     */
-    public void addBook(Book book) {
-        String isbn = book.getISBN();
-        if (books.containsKey(isbn)) {
-            // book already exists, increment the number of copies
-            Book existingBook = books.get(isbn);
-            existingBook.addCopies(book.getNumberOfCopies());
-        } else {
-            // book doesnt exist in the library. Add it to the library.
-            books.put(isbn, book);
-        }
-    }
+	/**
+	 * Adds a book to the library. If the library already has this book then it adds
+	 * the number of copies the library has.
+	 */
+	public void addBook(Book book) {
+		String isbn = book.getISBN();
+		if (books.containsKey(isbn)) {
+			// book already exists, increment the number of copies
+			Book existingBook = books.get(isbn);
+			existingBook.addCopies(book.getNumberOfCopies());
+		} else {
+			// book doesnt exist in the library. Add it to the library.
+			books.put(isbn, book);
+		}
+	}
 
-    /**
-     * Checks out the given book from the library. Throw the appropriate
-     * exception if book doesnt exist or there are no more copies available.
-     */
-    public void checkout(String isbn) {
-        // TODO: Implement this method.
-        throw new UnsupportedOperationException("not implemented");
-    }
+	/**
+	 * Checks out the given book from the library. Throw the appropriate exception
+	 * if book doesnt exist or there are no more copies available.
+	 */
+	public void checkout(String isbn) {
+		// TODO: Implement this method.
+		throw new UnsupportedOperationException("not implemented");
+	}
 
-    /**
-     * Returns a book to the library
-     */
-    public void returnBook(String isnb) {
-        // TODO: Implement this method.
-        throw new UnsupportedOperationException("not implemented");
-    }
+	/**
+	 * Returns a book to the library
+	 */
+	public void returnBook(String isnb) {
+		// TODO: Implement this method.
+		throw new UnsupportedOperationException("not implemented");
+	}
 
-    /**
-     * Finds this book in the library. Throws appropriate exception if the book
-     * doesnt exist.
-     */
-    public Book findByTitleAndAuthor(String title, String author) {
-        // TODO: Implement this method.
-        throw new UnsupportedOperationException("not implemented");
-    }
+	/**
+	 * Finds this book in the library. Throws appropriate exception if the book
+	 * doesnt exist.
+	 */
+	public Book findByTitleAndAuthor(String title, String author) {
+		// TODO: Implement this method.
+		throw new UnsupportedOperationException("not implemented");
+	}
 
-    /**
-     * Finds this book in the library. Throws appropriate exception if the book
-     * doesnt exist.
-     */
-    public Book findByISBN(String isbn) {
-        // TODO: Implement this method.
-        throw new UnsupportedOperationException("not implemented");
-    }
+	/**
+	 * Finds this book in the library. Throws appropriate exception if the book
+	 * doesnt exist.
+	 */
+	public Book findByISBN(String isbn) {
+		// TODO: Implement this method.
+		throw new UnsupportedOperationException("not implemented");
+	}
 
-    /**
-     * Saves the contents of this library to the given file.
-     */
-    public void save(String filename) {
-        try {
-        	PrintWriter writer = new PrintWriter(filename);
-        	
-        	for (Book book : booksByIsbn.values()) {
-        		writer.println(
-        				book.getTitle() + " " + 
-        		        book.getAuthor() + " " + 
-        				book.getIsbn() + " " + 
-        		        book.getPublicationYear() + " " + 
-        				book.getNumberOfCopies() + " " + 
-        		        book.getAvailableCopies()
-        			);
-        	}
-        	
-        	writer.close();
-        } catch (FileNotFoundException e) {
-        	throw new RuntimeException(
-        			"Could not save file: " + filename);
-        }
-    }
+	/**
+	 * Saves the contents of this library to the given file.
+	 */
+	public void save(String filename) {
+		try {
+			PrintWriter writer = new PrintWriter(filename);
 
-    /**
-     * Loads the contents of this library from the given file. All existing data
-     * in this library is cleared before loading from the file.
-     */
-    public void load(String filename) {
-        try {
-        	Scanner scanner = new Scanner(new File(filename));
-        	
-        	booksByIsbn.clear();
-        	
-        	while (scanner.hasNext()) {
-        		String title = scanner.next();
-        		String author = scanner.next();
-        		String isbn = scanner.next();
-        		int publicationYear = scanner.nextInt();
-        		int numberOfCopies = scanner.nextInt();
-        		int availableCopies = scanner.nextInt();
-        		
-        		Book book = new Book(title, author, isbn, publicationYear, numberOfCopies);
-        		book.setAvailableCopies(availableCopies);
-        		
-        		booksByIsbn.put(isbn, book);
-        	}
-        	
-        	scanner.close();
-        } catch (FileNotFoundException e) {
-        	throw new RuntimeException(
-        			"Could not load file: " + filename);
-        }
-    }
+			for (Book book : booksByIsbn.values()) {
+				writer.println(book.getTitle() + " " + book.getAuthor() + " " + book.getIsbn() + " "
+						+ book.getPublicationYear() + " " + book.getNumberOfCopies() + " " + book.getAvailableCopies());
+			}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+			writer.close();
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(
+					"Could not save file: " + filename);
+		}
+	}
 
-        while (true) {
-            System.out.print("library> ");
-            String line = scanner.nextLine();
-            // TODO: Implement code
-            if (line.startsWith("add")) {
-                // TODO: Implement this case.
-                // The format of the line is
-                // add title author isbn publicationYear numberOfCopies
-                // e.g. add Star_Trek Gene_Roddenberry ISBN-1234 1965 10
-                // NOTE: If a book already exists in the library, then the
-                // number of copies should be incremented by this amount.
-                // Do appropriate error checking here.
-            } else if (line.startsWith("checkout")) {
-                // TODO: Implement this case.
-                // The format of the line is
-                // checkout isbn
-                // e.g. checkout ISBN-1234
-                // NOTE: If the book doesnt exist in the library, then the code
-                // should print an error.
-            } else if (line.startsWith("findByTitleAndAuthor")) {
-                // TODO: Implement this case.
-                // The format of the line is
-                // findByTitleAndAuthor <title> <author>
-                // e.g. findByTitleAndAuthor Star_Trek Gene_Roddenberry
-                // NOTE: If the book doesnt exist in the library, then the code
-                // should print an error.
-                // If the book exists in the library, this code should print the
-                // ISBN, number of copies in the library, and the number of
-                // copies availabvle
-            } else if (line.startsWith("return")) {
-                // TODO: Implement this case.
-                // Format of the line is
-                // return <isbn>
-                // e.g. return ISBN-1234
-                // NOTE: If the book was never checked out, this code should
-                // print an error.
-            } else if (line.startsWith("list")) {
-                // TODO: Implement this case.
-                // Format of the line is
-                // list <isnb>
-                // e.g. list ISBN-1234
-                // NOTE: This code should print out the number of copies in the
-                // library and the number of copies available.
-            } else if (line.startsWith("save")) {
-                // TODO: Implement this case.
-                // Format of the line is
-                // save <filename>
-                // e.g. save LbraryFile.dat
-            } else if (line.startsWith("load")) {
-                // TODO: Implement this case.
-                // Format of the line is:
-                // load <filename>
-                // e.g. load LibraryFile.dat
-            } else if (line.startsWith("exit")) {
-                break;
-            }
-        }
-    }
+	/**
+	 * Loads the contents of this library from the given file. All existing data in
+	 * this library is cleared before loading from the file.
+	 */
+	public void load(String filename) {
+		try {
+			Scanner scanner = new Scanner(new File(filename));
+
+			booksByIsbn.clear();
+
+			while (scanner.hasNext()) {
+				String title = scanner.next();
+				String author = scanner.next();
+				String isbn = scanner.next();
+				int publicationYear = scanner.nextInt();
+				int numberOfCopies = scanner.nextInt();
+				int availableCopies = scanner.nextInt();
+
+				Book book = new Book(
+						title, author, isbn, publicationYear, numberOfCopies);
+				book.setAvailableCopies(availableCopies);
+
+				booksByIsbn.put(isbn, book);
+			}
+
+			scanner.close();
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(
+					"Could not load file: " + filename);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("library> ");
+			String line = scanner.nextLine();
+			// TODO: Implement code
+			if (line.startsWith("add")) {
+				// TODO: Implement this case.
+				// The format of the line is
+				// add title author isbn publicationYear numberOfCopies
+				// e.g. add Star_Trek Gene_Roddenberry ISBN-1234 1965 10
+				// NOTE: If a book already exists in the library, then the
+				// number of copies should be incremented by this amount.
+				// Do appropriate error checking here.
+			} else if (line.startsWith("checkout")) {
+				// TODO: Implement this case.
+				// The format of the line is
+				// checkout isbn
+				// e.g. checkout ISBN-1234
+				// NOTE: If the book doesnt exist in the library, then the code
+				// should print an error.
+			} else if (line.startsWith("findByTitleAndAuthor")) {
+				// TODO: Implement this case.
+				// The format of the line is
+				// findByTitleAndAuthor <title> <author>
+				// e.g. findByTitleAndAuthor Star_Trek Gene_Roddenberry
+				// NOTE: If the book doesnt exist in the library, then the code
+				// should print an error.
+				// If the book exists in the library, this code should print the
+				// ISBN, number of copies in the library, and the number of
+				// copies availabvle
+			} else if (line.startsWith("return")) {
+				// TODO: Implement this case.
+				// Format of the line is
+				// return <isbn>
+				// e.g. return ISBN-1234
+				// NOTE: If the book was never checked out, this code should
+				// print an error.
+			} else if (line.startsWith("list")) {
+				// TODO: Implement this case.
+				// Format of the line is
+				// list <isnb>
+				// e.g. list ISBN-1234
+				// NOTE: This code should print out the number of copies in the
+				// library and the number of copies available.
+			} else if (line.startsWith("save")) {
+				// TODO: Implement this case.
+				// Format of the line is
+				// save <filename>
+				// e.g. save LbraryFile.dat
+			} else if (line.startsWith("load")) {
+				// TODO: Implement this case.
+				// Format of the line is:
+				// load <filename>
+				// e.g. load LibraryFile.dat
+			} else if (line.startsWith("exit")) {
+				break;
+			}
+		}
+	}
 }
