c056f1185b298dcc7e415f5c4083fe74efca43af
Joey32100 <monteonjoey@yahoo.com>
2026-05-17 17:16:54 -0700

    Implement add functionality to shell, and remove book implementation (#30)
    * Implement add and remove book functionality
    
    * Update src/library/Library.java
    
    * Updated code based on review suggestions
    
    * Add missing Javadoc param tag
    
    Reviewed and fixed conflicts: Eric Chevrie <itsmiyamiiyaa@gmail.com>

--- src/library/Library.java
+++ src/library/Library.java
@@ -30,6 +30,7 @@ public class Library {
      */
     public void addBook(Book book) {
         String isbn = book.getISBN();
+
         if (books.containsKey(isbn)) {
             // book already exists, increment the number of copies
             Book existingBook = books.get(isbn);
@@ -38,6 +39,20 @@ public class Library {
             // book doesnt exist in the library. Add it to the library.
             books.put(isbn, book);
         }
+    }
+
+   /**
+ * Removes a book from the library using its ISBN.
+ *
+ * @param isbn the ISBN of the book to remove
+ */
+    public void removeBook(String isbn) {
+        if (!books.containsKey(isbn)) {
+            throw new java.util.NoSuchElementException(
+                    "Book with ISBN " + isbn + " does not exist.");
+        }
+
+        books.remove(isbn);
     }
 
     /**
@@ -151,19 +166,84 @@ public class Library {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Library lib = new Library();
+        Library library = new Library();
+
         while (true) {
             System.out.print("library> ");
             String line = scanner.nextLine();
             // TODO: Implement code
             if (line.startsWith("add")) {
-                // TODO: Implement this case.
                 // The format of the line is
                 // add title author isbn publicationYear numberOfCopies
                 // e.g. add Star_Trek Gene_Roddenberry ISBN-1234 1965 10
                 // NOTE: If a book already exists in the library, then the
                 // number of copies should be incremented by this amount.
-                // Do appropriate error checking here.
+
+                try {
+                    String[] parts = line.split("\\s+");
+
+                    if (parts.length != 6) {
+                        System.out.println(
+                                "Error: add format is add title author "
+                                + "isbn publicationYear numberOfCopies");
+                    } else {
+                        String title = parts[1];
+                        String author = parts[2];
+                        String isbn = parts[3];
+
+                        int publicationYear =
+                                Integer.parseInt(parts[4]);
+
+                        int numberOfCopies =
+                                Integer.parseInt(parts[5]);
+
+                        Book book = new Book(
+                                title,
+                                author,
+                                isbn,
+                                publicationYear,
+                                numberOfCopies);
+
+                        library.addBook(book);
+
+                        System.out.println(
+                                "Book added successfully.");
+                    }
+
+                } catch (NumberFormatException e) {
+                    System.out.println(
+                            "Error: publication year and number "
+                            + "of copies must be numbers.");
+                } catch (RuntimeException e) {
+                    System.out.println(
+                            "Error: " + e.getMessage());
+                }
+
+            } else if (line.startsWith("remove")) {
+                // Format of the line is
+                // remove <isbn>
+                // e.g. remove ISBN-1234
+
+                try {
+                    String[] parts = line.split("\\s+");
+
+                    if (parts.length != 2) {
+                        System.out.println(
+                                "Error: remove format is remove isbn");
+                    } else {
+                        String isbn = parts[1];
+
+                        library.removeBook(isbn);
+
+                        System.out.println(
+                                "Book removed successfully.");
+                    }
+
+                } catch (RuntimeException e) {
+                    System.out.println(
+                            "Error: " + e.getMessage());
+                }
+
             } else if (line.startsWith("checkout")) {
                 // TODO: Implement this case.
                 // The format of the line is
@@ -171,6 +251,7 @@ public class Library {
                 // e.g. checkout ISBN-1234
                 // NOTE: If the book doesnt exist in the library, then the code
                 // should print an error.
+
             } else if (line.startsWith("findByTitleAndAuthor")) {
                 // TODO: Implement this case.
                 // The format of the line is
@@ -181,6 +262,7 @@ public class Library {
                 // If the book exists in the library, this code should print the
                 // ISBN, number of copies in the library, and the number of
                 // copies availabvle
+
             } else if (line.startsWith("return")) {
                 // TODO: Implement this case.
                 // Format of the line is
@@ -188,6 +270,7 @@ public class Library {
                 // e.g. return ISBN-1234
                 // NOTE: If the book was never checked out, this code should
                 // print an error.
+
             } else if (line.startsWith("list")) {
                 // TODO: Implement this case.
                 // Format of the line is
@@ -195,6 +278,7 @@ public class Library {
                 // e.g. list ISBN-1234
                 // NOTE: This code should print out the number of copies in the
                 // library and the number of copies available.
+
             } else if (line.startsWith("save")) {
                 // TODO: Implement this case.
                 // Format of the line is
@@ -207,7 +291,7 @@ public class Library {
                 if (!filename.endsWith(".json")) {
                     filename += ".json";
                 }
-                lib.save(filename);
+                library.save(filename);
 
             } else if (line.startsWith("load")) {
                 // TODO: Implement this case.
@@ -221,7 +305,7 @@ public class Library {
                     filename += ".json";
                 }
 
-                lib.load(filename);
+                library.load(filename);
 
             } else if (line.startsWith("exit")) {
                 break;
